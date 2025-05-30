1ce4eafd1f76274b4855557f7a95c1625c056429
Rafael Morais <contato@rafaelmoraisjr.com>
2025-05-30 14:08:05 -0300

    permitir uso sem authorization do endpoint do viacep

--- src/main/java/com/avaliacaoimoveis/config/SecurityConfig.java
+++ src/main/java/com/avaliacaoimoveis/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/api/ceps/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
--- src/main/java/com/avaliacaoimoveis/repository/UsuarioRepository.java
+++ src/main/java/com/avaliacaoimoveis/repository/UsuarioRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-//public interface UsuarioRepository {
-//}
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
